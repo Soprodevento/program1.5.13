2daa466ca89dc78fdecbcc5c2a20ea0b75957e28
Soprodevento <Sashinka2803@gmail.com>
2024-03-15 00:52:00 +0300

    Убрал if

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
+import java.util.StringJoiner;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 20, 8, 7, 3, 100};
+        int[] arr = {3,5,20,8,7,3,100};
         printOddNumbers(arr);
     }
 
-    public static void printOddNumbers(int[] arr) {
-        StringBuilder oddNumbers = new StringBuilder();
-        for (int num : arr) {
+    public static void printOddNumbers(int[] numbers) {
+        StringJoiner oddNumbers = new StringJoiner(", ");
+
+        for (int num : numbers) {
             if (num % 2 != 0) {
-                if (oddNumbers.length() > 0) {
-                    oddNumbers.append(",");
-                }
-                oddNumbers.append(num);
+                oddNumbers.add(String.valueOf(num));
             }
         }
-        System.out.println(oddNumbers.toString());
+
+        System.out.println(oddNumbers);
     }
 }
